018ce51b1f6702d26e568e8e4d35f5dfa5de31c1
Abhishek shukla <abhishekshukla0700@gmail.com>
2022-02-14 02:18:23 +0530

    one

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
-var product = [];
+// var product = [];
+var products = [{
+        "id": "100",
+        "name": "iPhone 4S",
+        "brand": "Apple",
+        "os": "iOS"
+    },
+    {
+        "id": "101",
+        "name": "Moto X",
+        "brand": "Motorola",
+        "os": "Android"
+    },
+    {
+        "id": "102",
+        "name": "iPhone 6",
+        "brand": "Apple",
+        "os": "iOS"
+    },
+    {
+        "id": "103",
+        "name": "Samsung Galaxy S",
+        "brand": "Samsung",
+        "os": "Android"
+    },
+    {
+        "id": "104",
+        "name": "Google Nexus",
+        "brand": "ASUS",
+        "os": "Android"
+    },
+    {
+        "id": "105",
+        "name": "Surface",
+        "brand": "Microsoft",
+        "os": "Windows"
+    }
+];
 flag = 1;
 $(document).ready(function() {
     $('#Button').click(function() {
